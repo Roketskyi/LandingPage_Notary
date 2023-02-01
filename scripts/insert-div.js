fedcb7c6b3ecd21b2da52556e0d41988cc0e3eef
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2023-02-02 00:51:38 +0200

    update v1.4

---
+++ scripts/insert-div.js
@@ -0,0 +1,5 @@
+let div = document.createElement('div');
+
+div.innerHTML = "<a href='#contacts' class='header-btn'><strong>Безкоштовна консультація</strong></a>";
+
+document.documentElement.clientWidth > 576 ? divv.append(div) : phone.append(div);
